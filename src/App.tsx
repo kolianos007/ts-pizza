7588914d8e931f70f0a2402a98cc799ae69447f8
Kolya <kolianos007@gmail.com>
2021-12-23 18:25:51 +0200

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Header } from "./components";
 import { Routes, Route } from "react-router-dom";
 import { Cart, Home } from "./pages";
+import axios from "axios";
+
+interface IPizza {
+  id: number;
+  imageUrl: string;
+  name: string;
+  types: Array<number>;
+  sizes: Array<number>;
+  price: number;
+  category: number;
+  rating: number;
+}
 
 function App() {
+  const [pizzas, setPizzas] = useState<Array<IPizza>>([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/db.json")
+      .then((response) => setPizzas(response));
+  });
   return (
     <div className="App">
       <div className="wrapper">
